18a7d5110d22a680a5bb058aea56632b7a8f7fc4
Buttars <buttars0070@gmail.com>
2022-11-01 16:49:25 -0600

    setup inital custom config

---
+++ lua/custom/plugins/overrides.lua
@@ -0,0 +1,56 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "typescript",
+    "c",
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  open_on_setup = true,
+  open_on_setup_file = true,
+  open_on_tab = true,
+
+  hijack_directories = {
+    enable = true,
+    auto_open = true
+  },
+
+
+  git = {
+    folder_arrow = false,
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
+
